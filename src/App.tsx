83f56946a4ea5ed16cf41a3d319c556393d3135e
Nic <42087659+nicm42@users.noreply.github.com>
2021-07-18 11:52:27 +0100

    Added API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
+import { useEffect } from 'react';
+
 const App = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const link =
+          'https://public-api.wordpress.com/rest/v1.1/sites/nictesting935058505.wordpress.com/posts/?pretty=true';
+        const request = await fetch(link);
+        if (request.ok) {
+          const data = await request.json();
+          console.log(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return <></>;
 };
 
