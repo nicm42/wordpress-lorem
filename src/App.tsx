75740fa3fba8122872d9b2f29a17ecf9d7a663dc
Nic <42087659+nicm42@users.noreply.github.com>
2021-07-18 14:16:49 +0100

    Added the posts into the accordion

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,36 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Post from './components/Post';
 
 const App = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const link =
-          'https://public-api.wordpress.com/rest/v1.1/sites/nictesting935058505.wordpress.com/posts/?pretty=true';
-        const request = await fetch(link);
-        if (request.ok) {
-          const data = await request.json();
-          console.log(data.posts[0].title);
-          console.log(data.posts[0].content);
-        }
-      } catch (error) {
-        console.log(error);
+  const [posts, setPosts] = useState<any[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const link =
+        'https://public-api.wordpress.com/rest/v1.1/sites/nictesting935058505.wordpress.com/posts/?pretty=true';
+      const request = await fetch(link);
+      if (request.ok) {
+        const data = await request.json();
+        setPosts(data.posts);
+        console.log(data.posts);
+        //console.log(data.posts[0].title);
+        //console.log(data.posts[0].content);
       }
-    };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
     fetchData();
   }, []);
 
   return (
     <>
-      <Post />
+      {posts.map((post: any[]) => (
+        <Post post={post} key={post.ID} />
+      ))}
     </>
   );
 };
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,30 +1,32 @@
+import PropTypes from 'prop-types';
 import { Accordion, Card } from 'react-bootstrap';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-const Post = () => {
+interface PostProps {
+  post: any;
+}
+
+const Post = ({ post }: PostProps) => {
   return (
     <Accordion>
       <Card>
         <Accordion.Toggle as={Card.Header} eventKey="0">
-          TAB 1
+          {post.title}
         </Accordion.Toggle>
 
         <Accordion.Collapse eventKey="0">
-          <Card.Body>This is first tab body</Card.Body>
-        </Accordion.Collapse>
-      </Card>
-
-      <Card>
-        <Accordion.Toggle as={Card.Header} eventKey="1">
-          TAB 2
-        </Accordion.Toggle>
-
-        <Accordion.Collapse eventKey="1">
-          <Card.Body>This is second tab body</Card.Body>
+          <Card.Body>{post.content}</Card.Body>
         </Accordion.Collapse>
       </Card>
     </Accordion>
   );
+};
+
+Post.propTypes = {
+  post: PropTypes.shape({
+    ID: PropTypes.number,
+    title: PropTypes.string,
+    content: PropTypes.string,
+  }).isRequired,
 };
 
 export default Post;
