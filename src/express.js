da75dc5974efadaf8a7e8921660c8d356c3e6abd
Nic <42087659+nicm42@users.noreply.github.com>
2021-07-19 21:04:33 +0100

    Fetched Unsplash photo in Express

---
+++ src/express.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const fetch = require('node-fetch');
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+
+const link = 'https://api.unsplash.com';
+
+const key = process.env.REACT_APP_API_KEY;
+
+app.get('/photo', async (req, res) => {
+  const photo = await fetch(`${link}/photos/random?client_id=${key}&count=1`);
+  console.log(photo);
+});
