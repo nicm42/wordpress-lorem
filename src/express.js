731351c0fdb30f139ad73b02b61d09338b5ccccd
Nic <42087659+nicm42@users.noreply.github.com>
2021-07-19 21:08:48 +0100

    Fetched a random cake photo

--- src/express.js
+++ src/express.js
@@ -13,7 +13,10 @@ const link = 'https://api.unsplash.com';
 
 const key = process.env.REACT_APP_API_KEY;
 
-app.get('/photo', async (req, res) => {
-  const photo = await fetch(`${link}/photos/random?client_id=${key}&count=1`);
-  console.log(photo);
+app.get('/cake', async (req, res) => {
+  const photo = await fetch(
+    `${link}/photos/random?client_id=${key}&count=1&query=cake`
+  );
+  const photoResponse = await photo.json();
+  console.log(photoResponse);
 });
