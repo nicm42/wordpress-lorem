d88c024025e75f31b194826741245314ef330ada
Nic <42087659+nicm42@users.noreply.github.com>
2021-07-21 20:56:43 +0100

    Updated Express to TypeScript

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
+import express, { Request, Response } from 'express';
 require('dotenv').config();
 
-const express = require('express');
 const app = express();
 const port = 8000;
 const fetch = require('node-fetch');
@@ -13,7 +13,7 @@ const link = 'https://api.unsplash.com';
 
 const key = process.env.REACT_APP_API_KEY;
 
-app.get('/knight', async (req, res) => {
+app.get('/knight', async (req: Request, res: Response) => {
   const photo = await fetch(
     `${link}/photos/random?client_id=${key}&count=1&query=knight`
   );
@@ -21,7 +21,7 @@ app.get('/knight', async (req, res) => {
   res.send(photoResponse);
 });
 
-app.get('/space', async (req, res) => {
+app.get('/space', async (req: Request, res: Response) => {
   const photo = await fetch(
     `${link}/photos/random?client_id=${key}&count=1&query=space`
   );
@@ -29,7 +29,7 @@ app.get('/space', async (req, res) => {
   res.send(photoResponse);
 });
 
-app.get('/cat', async (req, res) => {
+app.get('/cat', async (req: Request, res: Response) => {
   const photo = await fetch(
     `${link}/photos/random?client_id=${key}&count=1&query=cat`
   );
@@ -37,10 +37,13 @@ app.get('/cat', async (req, res) => {
   res.send(photoResponse);
 });
 
-app.get('/cake', async (req, res) => {
+app.get('/cake', async (req: Request, res: Response) => {
   const photo = await fetch(
     `${link}/photos/random?client_id=${key}&count=1&query=cake`
   );
   const photoResponse = await photo.json();
   res.send(photoResponse);
 });
+
+// So TypeScript is happy about the dotenv config
+export {};
