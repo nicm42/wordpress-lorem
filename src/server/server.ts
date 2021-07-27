636e84efbf63a6c8b5bade2571991bdc5199cf6b
Nic <42087659+nicm42@users.noreply.github.com>
2021-07-27 17:03:45 +0100

    Updated Express so it stops being in the ts-node error loop

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,51 +1,26 @@
-import getImage from './getImage';
-
 import express, { Request, Response } from 'express';
-//import axios from 'axios';
+import axios from 'axios';
 require('dotenv').config();
+
+require('dotenv').config();
+
+const link = 'https://api.unsplash.com';
+const key = process.env.REACT_APP_API_KEY;
 
 const app = express();
 const port = process.env.PORT || 8000;
-//const fetch = require('node-fetch');
 
-app.listen(port, () => {
+module.exports = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
 
-/* const link = 'https://api.unsplash.com';
-const key = process.env.REACT_APP_API_KEY; */
-
 app.get('/knight', async (req: Request, res: Response) => {
-  const photoResponse = await getImage('knight');
-  console.log(photoResponse);
-  res.send(photoResponse);
-});
-
-app.get('/space', async (req: Request, res: Response) => {
-  const photoResponse = await getImage('space');
-  console.log(photoResponse);
-  res.send(photoResponse);
-});
-
-app.get('/cat', async (req: Request, res: Response) => {
-  const photoResponse = await getImage('cat');
-  console.log(photoResponse);
-  res.send(photoResponse);
-});
-
-app.get('/cake', async (req: Request, res: Response) => {
-  const photoResponse = await getImage('cake');
-  console.log(photoResponse);
-  res.send(photoResponse);
-});
-
-/* app.get('/knight', async (req: Request, res: Response) => {
   const photo = await axios.get(
     `${link}/photos/random?client_id=${key}&count=1&query=knight`
   );
   const photoResponse = await photo;
   console.log(photoResponse);
-  res.send(photoResponse);
+  res.send(photoResponse.data);
 });
 
 app.get('/space', async (req: Request, res: Response) => {
@@ -53,7 +28,8 @@ app.get('/space', async (req: Request, res: Response) => {
     `${link}/photos/random?client_id=${key}&count=1&query=space`
   );
   const photoResponse = await photo;
-  res.send(photoResponse);
+  //console.log(photoResponse);
+  res.send(photoResponse.data);
 });
 
 app.get('/cat', async (req: Request, res: Response) => {
@@ -61,7 +37,8 @@ app.get('/cat', async (req: Request, res: Response) => {
     `${link}/photos/random?client_id=${key}&count=1&query=cat`
   );
   const photoResponse = await photo;
-  res.send(photoResponse);
+  //console.log(photoResponse);
+  res.send(photoResponse.data);
 });
 
 app.get('/cake', async (req: Request, res: Response) => {
@@ -69,7 +46,9 @@ app.get('/cake', async (req: Request, res: Response) => {
     `${link}/photos/random?client_id=${key}&count=1&query=cake`
   );
   const photoResponse = await photo;
-  res.send(photoResponse);
-}); */
+  //console.log(photoResponse);
+  res.send(photoResponse.data);
+});
 
-module.exports = app;
+//module.exports = app;
+//export {};
